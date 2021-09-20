c7cabeecba6ee27e1ffa62cac6af86bd9830d5ea
Mao <m@maowtm.org>
2021-09-20 20:37:30 +0100

    use ghcr

--- build-docker-images.sh
+++ build-docker-images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -xe
 
-docker build kube-tester -t registry.digitalocean.com/maowtm-images/kube-tester -f kube-tester/Dockerfile
-docker push registry.digitalocean.com/maowtm-images/kube-tester
-docker build custom-controllers/secret-copy -t registry.digitalocean.com/maowtm-images/secret-copy
-docker push registry.digitalocean.com/maowtm-images/secret-copy
+docker build kube-tester -t ghcr.io/micromaomao/kube-tester -f kube-tester/Dockerfile
+docker push ghcr.io/micromaomao/kube-tester
+docker build custom-controllers/secret-copy -t ghcr.io/micromaomao/secret-copy
+docker push ghcr.io/micromaomao/secret-copy
