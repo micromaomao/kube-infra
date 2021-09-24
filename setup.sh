f241acaf5d9d4deae26fe7d143b1e47e5cb985f8
Mao <m@maowtm.org>
2021-09-24 12:25:15 +0100

    Explicitly use default namespace

--- setup.sh
+++ setup.sh
@@ -17,10 +17,15 @@ helm upgrade --install \
 kubectl apply -f cert-issuer.yaml
 
 # nginx ingress
-helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx --values ingress-nginx.values.yaml
+helm upgrade --install \
+  ingress-nginx ingress-nginx/ingress-nginx \
+  --values ingress-nginx.values.yaml \
+  --namespace default
 kubectl apply -f ingress.yaml
 
 # kube metrics server
-helm upgrade --install metrics-server metrics-server/metrics-server
+helm upgrade --install \
+  metrics-server metrics-server/metrics-server \
+  --namespace default
 
 kubectl apply -R -f apps
